f3b27294b980c867d4178fb73394564fff11ae4e
MellowGuy <thotyl88@gmail.com>
2017-11-10 11:53:32 -0500

    Added movement to player

--- Bomb_Man_Project/Assets/Player.cs
+++ Bomb_Man_Project/Assets/Player.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour {
 
@@ -11,6 +9,33 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		GetInput();
+	}
+
+	private void GetInput()
+	{
+		//Left/Right movement
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.position += Vector3.left * Time.deltaTime;
+			Debug.Log("X position= " + transform.position.x);
+		}
+		else if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.position += Vector3.right * Time.deltaTime;
+			Debug.Log("X position= " + transform.position.x);
+		}
+		//Up/Down movement
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.position += Vector3.up * Time.deltaTime;
+			Debug.Log("Y position= " + transform.position.y);
+		}
+		else if (Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.position += Vector3.down * Time.deltaTime;
+			Debug.Log("Y position= " + transform.position.y);
+		}
+
 	}
 }
